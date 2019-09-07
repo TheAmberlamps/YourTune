9457000fe68d30b446bcfa87ac24c50db2c3ecd8
CristobalSalazar <chris.salza@gmail.com>
2019-09-07 12:45:41 -0400

    final pt 2

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -11,7 +11,7 @@ module.exports = function(app) {
     });
   });
 
-// Load example page and pass in an example by id
+  //Load example page and pass in an example by id
   app.get("/example/:id", function(req, res) {
     db.Example.findOne({ where: { id: req.params.id } }).then(function(
       dbExample
@@ -19,7 +19,7 @@ module.exports = function(app) {
       res.render("example", {
         example: dbExample
       });
-    });
+    });});
 
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
