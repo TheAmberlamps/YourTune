714f9a53a6c73e080a49f51bc848ceee1673411a
Paul Unsted <ed__84@hotmail.com>
2019-09-12 18:49:27 -0400

    Removed purchases column from the users table, it's no longer required.

--- models/seeds.sql
+++ models/seeds.sql
@@ -1,8 +1,8 @@
 USE app_db;
 
-INSERT INTO users (user_name, pass, email, card_num, purchases) VALUES ("Paul", "Paulpass", "unsted.paul@gmail.com", 123456789, "purchasejunk");
-INSERT INTO users (user_name, pass, email, card_num, purchases) VALUES ("Testboi", "Testpass", "boi.test@gmail.com", 987654321, "purchasejunk");
-INSERT INTO users (user_name, pass, email, card_num, purchases) VALUES ("Boitest", "Passtest", "test.boi@gmail.com", 000000000, "purchasejunk");
+INSERT INTO users (user_name, pass, email, card_num) VALUES ("Paul", "Paulpass", "unsted.paul@gmail.com", 123456789);
+INSERT INTO users (user_name, pass, email, card_num) VALUES ("Testboi", "Testpass", "boi.test@gmail.com", 987654321);
+INSERT INTO users (user_name, pass, email, card_num) VALUES ("Boitest", "Passtest", "test.boi@gmail.com", 000000000);
 
 INSERT INTO tracks (track_name, track_url_full, track_url_preview, u_id) VALUES ("dummyname", "dummyurl_full", "dummyurl_prev", 1);
 
