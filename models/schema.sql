eac67366447895b782ec87fefbd0256b9fac6d4b
Paul Unsted <ed__84@hotmail.com>
2019-09-12 19:23:37 -0400

    camel-cased database column names to conform with linter

--- models/schema.sql
+++ models/schema.sql
@@ -2,26 +2,26 @@ DROP DATABASE IF EXISTS app_db;
 CREATE DATABASE app_db;
 
 CREATE TABLE users (
-    user_id INT NOT NULL AUTO_INCREMENT,
-    user_name VARCHAR(50) NOT NULL,
+    userId INT NOT NULL AUTO_INCREMENT,
+    userName VARCHAR(50) NOT NULL,
     pass VARCHAR(50) NOT NULL,
     email VARCHAR(50) NOT NULL,
-    card_num INT(20) NOT NULL, 
-    PRIMARY KEY (user_id)
+    cardNum INT(20) NOT NULL, 
+    PRIMARY KEY (userId)
 );
 
 CREATE TABLE tracks (
-    track_id INT NOT NULL AUTO_INCREMENT,
-    track_name VARCHAR (250) NOT NULL,
-    track_url_full VARCHAR(250) NOT NULL,
-    track_url_preview VARCHAR(250) NOT NULL,
-    user_id INT FOREIGN KEY REFERENCES users(user_id),
-    PRIMARY KEY (track_id)
+    trackId INT NOT NULL AUTO_INCREMENT,
+    trackName VARCHAR (250) NOT NULL,
+    trackUrlFull VARCHAR(250) NOT NULL,
+    trackUrlPreview VARCHAR(250) NOT NULL,
+    userId INT FOREIGN KEY REFERENCES users(userId),
+    PRIMARY KEY (trackId)
 );
 
 CREATE TABLE purchases (
     id INT NOT NULL AUTO_INCREMENT,
-    u_id INT FOREIGN KEY REFERENCES users(user_id),
-    t_id INT FOREIGN KEY REFERENCES tracks(track_id),
+    uId INT FOREIGN KEY REFERENCES users(user_id),
+    tId INT FOREIGN KEY REFERENCES tracks(track_id),
     PRIMARY KEY (id)
 );
