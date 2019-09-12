e1b14edace176cdaba7cd46df3962c71b05162a0
Paul Unsted <ed__84@hotmail.com>
2019-09-12 01:06:23 -0400

    Ok, the schema flawlessly generates a database with the necessary tables and seeds populates it with enough data to test it.

---
+++ models/seeds.sql
@@ -0,0 +1,9 @@
+USE app_db;
+
+INSERT INTO users (user_name, pass, email, card_num, purchases) VALUES ("Paul", "Paulpass", "unsted.paul@gmail.com", 123456789, "purchasejunk");
+INSERT INTO users (user_name, pass, email, card_num, purchases) VALUES ("Testboi", "Testpass", "boi.test@gmail.com", 987654321, "purchasejunk");
+INSERT INTO users (user_name, pass, email, card_num, purchases) VALUES ("Boitest", "Passtest", "test.boi@gmail.com", 000000000, "purchasejunk");
+
+INSERT INTO tracks (track_name, track_url_full, track_url_preview, u_id) VALUES ("dummyname", "dummyurl_full", "dummyurl_prev", 1);
+
+INSERT INTO purchases (u_id, t_id) VALUES (1, 1);
