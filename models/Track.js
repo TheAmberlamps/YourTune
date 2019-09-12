536815e008c2054408a7434361289e8d22d618ad
TheAmberlamps <50154883+TheAmberlamps@users.noreply.github.com>
2019-09-12 18:59:12 -0400

    Update Track.js
    edited to match database values

--- models/Track.js
+++ models/Track.js
@@ -1,10 +1,10 @@
 module.exports = function(sequelize, DataTypes) {
   var Track = sequelize.define("track", {
-    id: DataTypes.INTEGER,
-    name: DataTypes.STRING,
-    preview: DataTypes.STRING,
-    download: DataTypes.STRING,
-    user: DataTypes.INTEGER
+    track_id: DataTypes.INTEGER,
+    track_name: DataTypes.STRING,
+    track_url_full: DataTypes.STRING,
+    track_url_preview: DataTypes.STRING,
+    u_id: DataTypes.INTEGER
   });
   return Track;
 };
