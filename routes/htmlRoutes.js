73c3d31693b891cf1628216e928eb1f45893f4eb
unknown <mannisingh182@gmail.com>
2019-09-14 11:24:55 -0500

    Added handlebars

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,26 +3,22 @@ var db = require("../models");
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
-    });
+    res.render("index");
   });
 
-  //Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(
-      dbExample
-    ) {
-      res.render("example", {
-        example: dbExample
-      });
-    });});
+  app.get("/topsong", function(req, res) {
+    res.render("topsong");
+  });
 
-  // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
-    res.render("404");
+  app.get("/favorites", function(req, res) {
+    res.render("favorites");
+  });
+
+  app.get("/buysong", function(req, res) {
+    res.render("buysong");
+  });
+
+  app.get("/contact", function(req, res) {
+    res.render("contact");
   });
 };
